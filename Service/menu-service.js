319dbec8ee856afcd04dd24027f56755482eae48
Daniel Freitas Ferreira <dan10ff@gmail.com>
2021-12-29 20:43:12 +0000

    Create menu-service.js

---
+++ Service/menu-service.js
@@ -0,0 +1,22 @@
+
+function getAllMenuItem() {
+    
+}
+
+function getMenuItem() {
+    
+}
+
+function insertMenuItem() {
+    
+}
+
+function updateMenuItem() {
+    
+}
+
+function deleteMenuItem() {
+    
+}
+
+module.exports = { getAllMenuItem, getMenuItem, insertMenuItem, updateMenuItem, deleteMenuItem };
