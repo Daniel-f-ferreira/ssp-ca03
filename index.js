f27e3d375332afaaa9a3cad3a3874fed201ada5b
Daniel Freitas Ferreira <dan10ff@gmail.com>
2022-01-06 11:29:48 +0000

    update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const   router = express(),
 router.use(express.static(path.resolve(__dirname,'views'))); //We serve static content from "views" folder
 router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
 router.use(express.json()); //We include support for JSON
+// test
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
     const addr = server.address();
